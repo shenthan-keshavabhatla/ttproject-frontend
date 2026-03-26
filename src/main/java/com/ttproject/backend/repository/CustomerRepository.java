ca36fe9c6ba73b53128dcbb158215ca824b9ad5e
Shenthan <23eg104c48@anurag.edu.in>
2026-03-26 21:32:34 +0530

    saving changes before pull

--- src/main/java/com/ttproject/backend/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ttproject.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.ttproject.backend.entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, String> {
-}
